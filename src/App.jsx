ab35c2ad419e9c8b739d169bf98a1baf4ef76bf9
flook25 <flook11223344@gmail.com>
2025-06-28 06:37:00 +0700

    commit first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Headers"
+
+function App() {
+  
+
+  return (
+    <div className="App">
+     
+     <Header />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Headers.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return(
+        <header>
+            <p>Shopping Application</p>
+            <p>The Amout :</p>
+        </header>
+    )
+}
